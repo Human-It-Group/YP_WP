52d4e4ecf5d656dc94a4583060bc744745b6f3aa
tarask030 <38398391+tarask030@users.noreply.github.com>
2021-04-29 11:46:23 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -57,88 +57,88 @@ Template Name: Мой шаблон страницы
                     </div>
                 </div>
                 <div class = "form_all"
-                <div class="form_part_left">
-                    <div class="name_form_first">
-                        <h3>
-                            Imię
-                        </h3>
+                    <div class="form_part_left">
+                        <div class="name_form_first">
+                            <h3>
+                                Imię
+                            </h3>
+                        </div>
+                        <input type="text" name="user_name" placeholder="Imię" class="n_input" required />
+                        <div class="name_form">
+                            <h3>
+                                Nazwisko
+                            </h3>
+                        </div>
+                        <input type="text" name="user_surname" placeholder="Nazwisko" class="n_input" required />
+                        <div class="name_form">
+                            <h3>
+                                Numer telefonu
+                            </h3>
+                        </div>
+                        <input type="text" name="user_phone" placeholder="Numer telefonu" class="n_input" required />
+                        <div class="name_form">
+                            <h3>
+                                E-mail
+                            </h3>
+                        </div>
+                        <input type="text" name="user_email" placeholder="E-mail" class="n_input" required />
                     </div>
-                    <input type="text" name="user_name" placeholder="Imię" class="n_input" required />
-                    <div class="name_form">
-                        <h3>
-                            Nazwisko
-                        </h3>
-                    </div>
-                    <input type="text" name="user_surname" placeholder="Nazwisko" class="n_input" required />
-                    <div class="name_form">
-                        <h3>
-                            Numer telefonu
-                        </h3>
-                    </div>
-                    <input type="text" name="user_phone" placeholder="Numer telefonu" class="n_input" required />
-                    <div class="name_form">
-                        <h3>
-                            E-mail
-                        </h3>
-                    </div>
-                    <input type="text" name="user_email" placeholder="E-mail" class="n_input" required />
-                </div>
-                <div class="form_part_right">
-                    <div class="name_form_right">
-                        <h3>
-                            Nazwa konta na instagramie
-                        </h3>
-                    </div>
-                    <input type="text" name="user_nikcname" placeholder="Nazwa konta na instagramie" class="n_input_right" required />
-                    <div class="name_form_right">
-                        <h3>
-                            Skąd dowiedziałe/aś się o rekrutacji?
-                        </h3>
-                    </div>
+                    <div class="form_part_right">
+                        <div class="name_form_right">
+                            <h3>
+                                Nazwa konta na instagramie
+                            </h3>
+                        </div>
+                        <input type="text" name="user_nikcname" placeholder="Nazwa konta na instagramie" class="n_input_right" required />
+                        <div class="name_form_right">
+                            <h3>
+                                Skąd dowiedziałe/aś się o rekrutacji?
+                            </h3>
+                        </div>
 
 
-                    <div class="radio_button">
-                        <div class="pretty p-icon p-round p-jelly">
-                            <input type="checkbox" name="instagram" value="Instagram"/>
-                            <div class="state ">
-                                <i class="icon mdi mdi-check" style="background-color: #FFB84B; margin-top: 6px;"></i>
-                                <label style="margin-left: 10px;">Instagram</label>
+                        <div class="radio_button">
+                            <div class="pretty p-icon p-round p-jelly">
+                                <input type="checkbox" name="instagram" value="Instagram"/>
+                                <div class="state ">
+                                    <i class="icon mdi mdi-check" style="background-color: #FFB84B; margin-top: 6px;"></i>
+                                    <label style="margin-left: 10px;">Instagram</label>
+                                </div>
                             </div>
-                        </div>
-                        <div class="pretty p-icon p-round p-jelly">
-                            <input type="checkbox" name="facebook" value="Facebook"/>
-                            <div class="state ">
-                                <i class="icon mdi mdi-check" style="background-color: #FFB84B; margin-top: 6px;"></i>
-                                <label style="margin-left: 10px;">Facebook</label>
+                            <div class="pretty p-icon p-round p-jelly">
+                                <input type="checkbox" name="facebook" value="Facebook"/>
+                                <div class="state ">
+                                    <i class="icon mdi mdi-check" style="background-color: #FFB84B; margin-top: 6px;"></i>
+                                    <label style="margin-left: 10px;">Facebook</label>
+                                </div>
                             </div>
-                        </div>
-                        <div class="pretty p-icon p-round p-jelly">
-                            <input type="checkbox" name="linkedin" value="Linkedin"/>
-                            <div class="state ">
-                                <i class="icon mdi mdi-check" style="background-color: #FFB84B; margin-top: 6px;"></i>
-                                <label style="margin-left: 10px;">Linkedin</label>
+                            <div class="pretty p-icon p-round p-jelly">
+                                <input type="checkbox" name="linkedin" value="Linkedin"/>
+                                <div class="state ">
+                                    <i class="icon mdi mdi-check" style="background-color: #FFB84B; margin-top: 6px;"></i>
+                                    <label style="margin-left: 10px;">Linkedin</label>
+                                </div>
                             </div>
-                        </div>
                         <div class="pretty p-icon p-round p-jelly">
                             <input type="checkbox" name="friends" value="Od znajomych"/>
                             <div class="state ">
                                 <i class="icon mdi mdi-check" style="background-color: #FFB84B; margin-top: 6px;"></i>
                                 <label style="margin-left: 10px;">Od znajomych</label>
                             </div>
-                        </div>
-                        <div class="pretty p-icon p-round p-jelly">
-                            <input type="checkbox" name="another" value="Inna odpowiedź"/>
-                            <div class="state ">
-                                <i class="icon mdi mdi-check" style="background-color: #FFB84B; margin-top: 6px;"></i>
-                                <label style="margin-left: 10px;">Inna odpowiedź</label>
+                            </div>
+                            <div class="pretty p-icon p-round p-jelly">
+                                <input type="checkbox" name="another" value="Inna odpowiedź"/>
+                                <div class="state ">
+                                    <i class="icon mdi mdi-check" style="background-color: #FFB84B; margin-top: 6px;"></i>
+                                    <label style="margin-left: 10px;">Inna odpowiedź</label>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="btn_next">
-                    <a onclick = "hideDiv()">DALEJ</a>
-                </div>
-                </div>
+                    <div class="btn_next">
+                        <a onclick = "hideDiv()">DALEJ</a>
+                    </div>
+                    </div>
                 </div>
 
 
