63d2ba12dec4ef9c4b0e931727b67330e01caaeb
tarask030 <38398391+tarask030@users.noreply.github.com>
2021-04-29 16:35:43 +0200

    Add files via upload

---
+++ cookies.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <title>YP landing page</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href= "style.css">
+      <link rel="preconnect" href="https://fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@500;600;700&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@700&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <a href="index.html" type = "submit">
+        <div class="logo_main"><img src="images/logo/YP_kolor.svg" alt="logo"></div>
+    </a>
+    <div class="thanku_text">
+      <h2>
+        Polityka</br><strong>COOKIES</strong>
+      </h2>
+    </div>
+    <div class="information">
+      <p>
+        1. Serwis nie zbiera w sposób automatyczny żadnych informacji, z wyjątkiem informacji zawartych w plikach cookies.</br>
+2. Pliki cookies (tzw. "ciasteczka") stanowią dane informatyczne, w szczególności pliki tekstowe, które przechowywane są w urządzeniu końcowym Użytkownika Serwisu i przeznaczone są do korzystania ze stron internetowych Serwisu. Cookies zazwyczaj zawierają nazwę strony internetowej, z której pochodzą, czas przechowywania ich na urządzeniu końcowym oraz unikalny numer.</br>
+3. Podmiotem zamieszczającym na urządzeniu końcowym Użytkownika Serwisu pliki cookies oraz uzyskującym do nich dostęp jest Human Services with Power z siedzibą w Lubin przy ul. Marii Skłodowskiej-Curie 91I, zarejestrowana pod numerem KRS0000764980. </br>
+4. Pliki cookies wykorzystywane są w celu: a) dostosowania zawartości stron internetowych Serwisu do preferencji Użytkownika oraz optymalizacji korzystania ze stron internetowych; w szczególności pliki pozwalają rozpoznać urządzenie Użytkownika Serwisu i odpowiednio wyświetlić stronę internetową, dostosowaną do jego indywidualnych potrzeb; b) tworzenia statystyk, które pomagają zrozumieć, w jaki sposób Użytkownicy Serwisu korzystają ze stron internetowych, co umożliwia ulepszanie ich struktury i zawartości; c) utrzymania sesji Użytkownika Serwisu (po zalogowaniu), dzięki której Użytkownik nie musi na każdej podstronie Serwisu ponownie wpisywać loginu i hasła. Takie cookies są wykorzystywane wyłączenie w celu realizacji zadań na rzecz użytkownika oraz są szyfrowane.</br>
+5. W ramach Serwisu stosowane są dwa zasadnicze rodzaje plików cookies: "sesyjne" (session cookies) oraz "stałe" (persistent cookies). Cookies "sesyjne" są plikami tymczasowymi, które przechowywane są w urządzeniu końcowym Użytkownika do czasu wylogowania, opuszczenia strony internetowej lub wyłączenia oprogramowania (przeglądarki internetowej). "Stale" pliki cookies przechowywane są w urządzeniu końcowym Użytkownika przez czas określony w parametrach plików cookies lub do czasu ich usunięcia przez Użytkownika.</br>
+6. W wielu przypadkach oprogramowanie służące do przeglądania stron internetowych (przeglądarka internetowa) domyślnie dopuszcza przechowywanie plików cookies w urządzeniu końcowym Użytkownika. Użytkownicy Serwisu mogą dokonać w każdym czasie zmiany ustawień dotyczących plików cookies. Ustawienia te mogą zostać zmienione w szczególności w taki sposób, aby blokować automatyczną obsługę plików cookies w ustawieniach przeglądarki internetowej bądź informować o ich każdorazowym zamieszczeniu w urządzeniu Użytkownika Serwisu. Szczegółowe informacje o możliwości i sposobach obsługi plików cookies dostępne są w ustawieniach oprogramowania (przeglądarki internetowej).</br>
+7. Operator Serwisu informuje, że ograniczenia stosowania plików cookies mogą wpłynąć na niektóre funkcjonalności dostępne na stronach internetowych Serwisu.</br>
+8. Na naszej stronie internetowej mogą znajdować się odwołania do innych serwisów internetowych, które stosują własne polityki prywatności. Rekomendujemy zapoznanie się z każdą z takich polityk, ponieważ Human Services with Power nie ponosi jakiejkolwiek odpowiedzialności za zasady zachowania prywatności obowiązujące na tych serwisach.</br>
+9. Pliki cookies zamieszczane w urządzeniu końcowym Użytkownika Serwisu mogą być również wykorzystywane przez współpracujących z operatorem Serwisu reklamodawców oraz partnerów.</br>
+10. Więcej informacji na temat plików cookies dostępnych jest pod adresem www.wszystkoociasteczkach.pl lub w sekcji "Pomoc" menu przeglądarki internetowej. Niniejsza Polityka Prywatności stanowi wartość intelektualną IAB Polska i jest zgodna z przepisami polskiego oraz międzynarodowego prawa.</br>
+
+      </p>
+    </div>
+    <div class="btn_thanku">
+      <a href="index.php" type = "submit">STRONA GLÓWNA</a>
+    </div>
+  </body>
+</html>
