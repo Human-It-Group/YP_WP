e4eb8b40db8e5c116968a7ec4dc6e4bbf6d019e1
tarask030 <38398391+tarask030@users.noreply.github.com>
2021-04-29 19:34:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -191,7 +191,7 @@
              <p>
                 Strona wykorzystuje pliki cookie. Jeśli nie wyrażasz zgody, ustawienia dotyczące plików cookies możesz zmienić w swojej przeglądarce. 
                 Pełna treść:
-                    <a href="/cookies.php">Polityka cookies</a>
+                    <a href="cookies.php">Polityka cookies</a>
                 </p>
              <div class="btn_cookies">
                  <button class="cookie-btn">
